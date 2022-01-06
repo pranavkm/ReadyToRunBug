b852dec2e53e103060022a01b3a2ed5d81a2db45
Pranav K <prkrishn@hotmail.com>
2022-01-06 14:55:13 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using c1;
+using System.Reflection;
+
+Console.WriteLine(typeof(Test_Æøå).Assembly.GetCustomAttributes());
---
+++ Test_Æøå.cs
@@ -0,0 +1,8 @@
+[assembly: System.Reflection.Metadata.MetadataUpdateHandler(typeof(c1.Test_Æøå))]
+
+namespace c1;
+
+internal sealed class Test_Æøå
+{
+
+}
